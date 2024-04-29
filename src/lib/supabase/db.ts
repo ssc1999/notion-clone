65f0864eb7901e8cc4e9e6f9dbed857f7ebfc8e8
Sergio <ssc1099.csmm@gmail.com>
2024-04-29 15:57:49 +0200

    feat: database schemas added

--- src/lib/supabase/db.ts
+++ src/lib/supabase/db.ts
@@ -3,20 +3,22 @@ import postgres from 'postgres';
 import * as dotenv from 'dotenv';
 import * as schema from '../../../migrations/schema';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import { max } from 'drizzle-orm';
 dotenv.config({ path: '.env' });
 
 if (!process.env.DATABASE_URL) {
-    console.log('no database URL');
+    console.log('No database URL');
 }
 
-const client = postgres(process.env.DATABASE_URL as string);
+const client = postgres(process.env.DATABASE_URL as string, {max:1});
 const db = drizzle(client, { schema });
 const migrateDb = async () => {
     try {
-        console.log('🟠 Migrating clinet');
+        console.log('🟠 Migrating client');
         await migrate(db, { migrationsFolder: "migrations" });
         console.log('🟢 Migration successfull');
     } catch (error) {
+        console.log(error)
         console.log('🔴 Error migrating the client');
     }
 }
